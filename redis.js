092d1b3f32f787924a9cf7fb645e757feada534f
Osman <1279406+pegasuspect@users.noreply.github.com>
2020-08-20 19:19:40 -0400

    version bump.

--- redis.js
+++ redis.js
@@ -1,19 +1,17 @@
+const redis = require('redis');
 var express = require("express");
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var app = express();
 var RedisStore = require('connect-redis')(session);
+let client = redis.createClient()
 
 app.use(cookieParser());
 app.use(session({
-	store: new RedisStore({
-		host: 'localhost',
-		port: 6379,
-		db: 2,
-		}),
+	store: new RedisStore({ client }),
 	secret: 'hello',
-  resave: true,
-  saveUninitialized: true
+	resave: true,
+	saveUninitialized: true
 }));
 
 app.get("/", function(req,res){
